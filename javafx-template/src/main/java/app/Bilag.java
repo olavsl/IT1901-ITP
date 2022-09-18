f901e0d8d68165886db7da7c26c5b9fa1bf23394
Eduard Prokhorikhin <eduardpr@stud.ntnu.no>
2022-09-18 15:30:15 +0200

    La til felt og basic metoder i Bilag.java

--- javafx-template/src/main/java/app/Bilag.java
+++ javafx-template/src/main/java/app/Bilag.java
@@ -1,5 +1,34 @@
 package app;
 
+import java.time.LocalDate;
+
 public class Bilag {
-    
+    private String name;
+    private double value;
+    private LocalDate date;
+    // kan legge til kategori ved seinere implementasjon
+
+    public Bilag(String name, double value) {
+        this.name = name;
+        this.value = value;
+        this.date = LocalDate.now();
+    }
+
+    public Bilag(String name, double value, LocalDate date) {
+        this.name = name;
+        this.value = value;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }    
 }
