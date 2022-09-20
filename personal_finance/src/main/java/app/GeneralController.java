a30a375428629cc684946f4f46437c72af1a67ff
Olav Selnes Lorentzen <olavslo@stud.ntnu.no>
2022-09-20 17:30:20 +0200

    Scene switching

--- personal_finance/src/main/java/app/GeneralController.java
+++ personal_finance/src/main/java/app/GeneralController.java
@@ -1,34 +1,19 @@
 package app;
 
-import java.io.IOException;
 import java.time.LocalDate;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import javafx.scene.Node;
 
-public class GeneralController {
+public class GeneralController extends SceneSwitcher {
 
-    @FXML
-    private TextField transactionAmount;
-
-    @FXML
-    private DatePicker transactionDate;
-
-    @FXML
-    private TextField transactionTitle;
+    @FXML private TextField transactionAmount;
+    @FXML private DatePicker transactionDate;
+    @FXML private TextField transactionTitle;
 
     private User user;
-
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
     
     @FXML
     void handleAddTransaction(ActionEvent event) {
@@ -45,17 +30,6 @@ public class GeneralController {
         }
         user.addTransaction(transaction);
         System.out.println(transaction.getTitle());
-    }
-
-    
-    //metode som bytter mellom scener
-    @FXML
-    public void switchToLogIn(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
     }
 
     public void setUser(User user) {
