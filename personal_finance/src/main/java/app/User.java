c79342527e07e3d36643a5f628606eae61ec6a4a
Zafewood <42995317+Zafewood@users.noreply.github.com>
2022-09-20 16:58:56 +0200

    added persistence by saving usernames in txt-file

--- personal_finance/src/main/java/app/User.java
+++ personal_finance/src/main/java/app/User.java
@@ -2,15 +2,17 @@ package app;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.io.*;
 
 public class User {
-    
+
     private String username;
     private String password;
     private List<Transaction> transactions = new ArrayList<>();
 
 
     public User(String username, String password, String confirmedPassword) {
+        checkIfUserExists(username);
         setUsername(username);
         setPassword(password, confirmedPassword);; // For later: Hash passord for more security
     }
@@ -35,17 +37,49 @@ public class User {
         if (!username.matches("[a-zA-Z0-9]+")) {
             throw new IllegalArgumentException("Username must only contain letters and numbers!");
         }
+        
+        if(!checkIfUserExists(username)){
+            this.username = username;
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter("src/main/resources/app/users.txt",true));
+                bw.newLine();
+                bw.write(username);
+                bw.close();
+            } catch (Exception ex) {
+                return;
+            }
+        }else{
+            throw new IllegalArgumentException("Username is already taken!");
+        }
+        
 
-        // if (username is already taken) {throw exception}
-
-        this.username = username;
     }
-
     private void setPassword(String password, String confirmedPassword) {
         if (!password.equals(confirmedPassword)) {
             throw new IllegalArgumentException("Passwords does not match!");
         }
 
         this.password = password;
+    }
+
+    private boolean checkIfUserExists(String username) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/app/users.txt"));
+            String s;
+            List<String> users = new ArrayList<String>();
+            while((s = br.readLine()) != null){
+                users.add(s);
+
+            }
+            br.close();
+            if(users.contains(username)){
+                return true;
+
+            }else{
+                return false;
+            }
+        }catch(Exception ex){
+            throw new IllegalArgumentException("File to read not found");
+        }
     }
 }
