dd005315def5214a70ac77c9c9d219ca69733d7a
olavsl <olav.selnes.lorentzen@hotmail.com>
2022-10-13 12:58:49 +0200

    Log in now satisfying MVC-principle.

--- personal_finance/fxui/src/main/java/personal_finance/ui/LogInController.java
+++ personal_finance/fxui/src/main/java/personal_finance/ui/LogInController.java
@@ -7,32 +7,26 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import personal_finance.core.PersonalFinanceModel;
 import personal_finance.core.User;
-import personal_finance.json.PersonalFinancePersistence;
-import personal_finance.util.PasswordHasher;
+import personal_finance.util.LogInAuthenticator;
 
 public class LogInController extends SceneSwitcher {
-
-    private PersonalFinancePersistence pfp = new PersonalFinancePersistence();
     
     @FXML private TextField username;
     @FXML private TextField password;
     @FXML private Label wrongLogInFeedback;
 
     @FXML
-    public void switchToOverview(ActionEvent event) throws IOException, NoSuchAlgorithmException {
-        pfp.setStorageFile("users.json");
-        PersonalFinanceModel model = pfp.loadPersonalFinanceModel();
+    public void logIn(ActionEvent event) throws IOException, NoSuchAlgorithmException {
+        String username = this.username.getText();
+        String password = this.password.getText();
+        
+        User user = LogInAuthenticator.logIn(username, password);
 
-        for (User user : model.getUsers()) {
-            if (username.getText().equals(user.getUsername()) && PasswordHasher.hash(password.getText()).equals(user.getPassword())) {
-                switchToOverview(event, user);
-                System.out.println("Successfully logged in!");
-                break;
-            }
+        if (user.equals(null)) {
+            wrongLogInFeedback.setText("Username and password are not matching!");
         }
-
-        wrongLogInFeedback.setText("Username and password are not matching!");
+        
+        switchToOverview(event, user);
     }
 }
