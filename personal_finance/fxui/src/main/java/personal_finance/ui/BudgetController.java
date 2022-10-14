95e9492db0ed6ae51eb07713db4407a1ef0bbd85
Eduard Prokhorikhin <eduardpr@stud.ntnu.no>
2022-10-14 12:12:16 +0200

    Changed Budget.fxml to comply with changes in release, duplicated the finished...

--- personal_finance/fxui/src/main/java/personal_finance/ui/BudgetController.java
+++ personal_finance/fxui/src/main/java/personal_finance/ui/BudgetController.java
@@ -16,35 +16,11 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import personal_finance.core.Transaction;
 import personal_finance.core.User;
 
-public class AddTransactionController extends SceneSwitcher {
+public class BudgetController extends SceneSwitcher {
 
-    @FXML private TextField transactionAmount;
-    @FXML private DatePicker transactionDate;
-    @FXML private TextField transactionTitle;
-    @FXML private Label userFeedback;
     @FXML private Label usernameDisplay;
 
-
     private User user;
-    
-    @FXML
-    void handleAddTransaction(ActionEvent event) {
-        LocalDate date = transactionDate.getValue();
-        String title = transactionTitle.getText();
-        double value = Double.valueOf(transactionAmount.getText());
-        Transaction transaction;
-
-        if (date==null) {
-            transaction = new Transaction(title, value);
-        }
-        else {
-            transaction = new Transaction(title, value, date);
-        }
-        user.addTransaction(transaction);
-        userFeedback.setText("Transaction added succesfully");
-
-    }
-
 
     public void setUser(User user) {
         this.user = user;
@@ -52,7 +28,7 @@ public class AddTransactionController extends SceneSwitcher {
 
     @FXML
     public void switchToLogIn(ActionEvent event) throws IOException {
-        switchToLogIn(event);
+        switchToLogIn(event, this.user);
     }
 
     @FXML
