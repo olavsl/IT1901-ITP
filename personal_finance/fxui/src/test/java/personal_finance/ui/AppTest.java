23d930abf1f26ede6134efce21c3b5f14c9317d5
Eduard Prokhorikhin <eduardpr@stud.ntnu.no>
2022-11-07 17:43:01 +0100

    Made a test for AddTransaction gui

--- personal_finance/fxui/src/test/java/personal_finance/ui/AppTest.java
+++ personal_finance/fxui/src/test/java/personal_finance/ui/AppTest.java
@@ -3,15 +3,23 @@ package personal_finance.ui;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import personal_finance.util.UserCreater;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
@@ -36,13 +44,13 @@ public class AppTest extends ApplicationTest {
 
     @BeforeEach
     public void makeUserAndLogIn() {
-// //creates user "test"
-//       clickOn("#signUpBtn");
-//       clickOn("#username").write("test");
-//       clickOn("#password").write("test");
-//       clickOn("#confirmedPassword").write("test");
-//       clickOn("#createBtn");
-//       clickOn("#logInBtn");
+//creates user "test"
+      clickOn("#signUpBtn");
+      clickOn("#username").write("test");
+      clickOn("#password").write("test");
+      clickOn("#confirmedPassword").write("test");
+      clickOn("#createBtn");
+      clickOn("#logInBtn");
         logIn();
     }
 
@@ -61,7 +69,38 @@ public class AppTest extends ApplicationTest {
           }
       }
 
-      private void logIn() {
+    @Test
+    public void testAddTransaction() {
+        logIn();
+
+        clickOn("#addBtn");
+
+        clickOn("#transactionTitle").write("test");
+        clickOn("#transactionAmount").write("100");
+        clickOn("#saveBtn");
+
+        Label userFeedback = (Label)lookup("#userFeedback");
+
+        assertTrue(userFeedback.getText().equals("Transaction added successfully"));
+
+        clickOn("#transactionTitle").write("");
+        clickOn("#transactionAmount").write("100");
+        clickOn("#saveBtn");
+
+        assertFalse(userFeedback.getText().equals("Transaction added successfully"));
+
+        clickOn("#transactionTitle").write("test");
+        clickOn("#transactionAmount").write("badInput");
+        clickOn("#saveBtn");
+
+        assertFalse(userFeedback.getText().equals("Transaction added successfully"));
+    }
+
+    @AfterEach
+    public void cleanUp() throws NoSuchAlgorithmException, IOException {
+        UserCreater.deleteUser("test", "users.json");
+    }
+    private void logIn() {
         //logs in with the user "test"
           clickOn("#username").write("test");
           clickOn("#password").write("test");
